ca5394e74cd95f4ebd69b1049613e75ec75bd094
Eduardo Garibay <dark.cloud2007@gmail.com>
2020-09-25 15:23:05 -0500

    Fix step0 to loop

--- impls/ed-cpp/step0_repl.cpp
+++ impls/ed-cpp/step0_repl.cpp
@@ -5,10 +5,17 @@ std::string EVAL(const std::string& evl);
 std::string PRINT(const std::string& msg);
 std::string rep(const std::string& input);
 
-int main(int arg1, std::string arg2)
+int main(int argi, char* argc[])
 {
     std::string userMessage = "user> ";
-    std::cout << rep(userMessage) << std::endl;
+    std::string input;
+    std::cout << userMessage << std::endl;
+
+    while (std::getline(std::cin, input))
+    {
+        std::cout << userMessage << std::endl;
+        std::cout << rep(input) << std::endl;
+    }
 
     return 0;
 }
