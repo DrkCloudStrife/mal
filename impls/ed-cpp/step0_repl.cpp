79c83c19bb34200e3988af122445ae1984547b9b
Eduardo Garibay <dark.cloud2007@gmail.com>
2020-09-25 17:32:33 -0500

    Ensure rep loop exists on ctrl+d

--- impls/ed-cpp/step0_repl.cpp
+++ impls/ed-cpp/step0_repl.cpp
@@ -4,20 +4,33 @@ std::string READ(const std::string& msg);
 std::string EVAL(const std::string& evl);
 std::string PRINT(const std::string& msg);
 std::string rep(const std::string& input);
+bool getUserPrompt(const std::string& prompt, std::string& input);
 
 int main(int argi, char* argc[])
 {
     std::string userMessage = "user> ";
     std::string input;
-    std::cout << userMessage << std::endl;
 
-    while (std::getline(std::cin, input))
+    while (getUserPrompt(userMessage, input))
     {
-        std::cout << userMessage << std::endl;
         std::cout << rep(input) << std::endl;
     }
 
     return 0;
+}
+
+bool getUserPrompt(const std::string& prompt, std::string& input)
+{
+    std::cout << prompt;
+    std::getline(std::cin, input);
+
+    if (std::cin.eof())
+    {
+        std::cout << std::endl;
+        return false;
+    }
+
+    return true;
 }
 
 std::string READ(const std::string& input)
